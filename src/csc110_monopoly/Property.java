f578e93987b567eea3538af0d7dc052890c9b3ff
lopace <lopace1@gmail.com>
2017-11-30 15:09:25 -0700

    modified constructer

--- src/csc110_monopoly/Property.java
+++ src/csc110_monopoly/Property.java
@@ -6,8 +6,11 @@ public class Property
 	
 	
 	private Properties name;
+	private int priceToBuy;
+	
 	private Colors color;
 	private boolean hasAllOfColor;
+	
 	
 	private int baseRent;
 	private int with1House;
@@ -23,7 +26,21 @@ public class Property
 	
 	
 	
-public Property(Properties name, int priceToBuy, Colors color, int baseRent, int with1House, int with2Houses, int with3Houses, int with4Houses, int withHotel, int morgageValue, int houseCost){
-		
+	public Property(Properties name, int priceToBuy, Colors color, int baseRent, int with1House, int with2Houses,
+			int with3Houses, int with4Houses, int withHotel, int mortgageValue, int houseCost,
+			boolean hasAllOfColor, boolean mortgaged){
+		this.name = name;
+		this.priceToBuy = priceToBuy;
+		this.color = color;
+		this.baseRent = baseRent;
+		this.with1House = with1House;
+		this.with2Houses = with2Houses;
+		this.with3Houses = with3Houses;
+		this.with4Houses = with4Houses;
+		this.withHotel = withHotel;
+		this.mortgageValue = mortgageValue;
+		this.houseCost = houseCost;
+		this.hasAllOfColor = hasAllOfColor;
+		this.mortgaged = mortgaged;
 	}
 }
