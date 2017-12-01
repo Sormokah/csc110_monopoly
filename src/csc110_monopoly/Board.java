35454fdb2157f2ea0542c6154c0fe0621f92382c
Dcraw139 <DCrawford@student.neumont.edu>
2017-12-01 16:15:41 -0700

    updating board class

--- src/csc110_monopoly/Board.java
+++ src/csc110_monopoly/Board.java
@@ -1,5 +1,18 @@
 package csc110_monopoly;
 
 public class Board {
-
+	private final int boardSize = 40;
+	private int [] board = new int [boardSize];
+	
+	
+	public int[] getBoard() {
+		return board;
+	}
+	public void setBoard(int[] board) {
+		this.board = board;
+	}
+	
+	private void initializeBoard() {
+		//fill the spots of the array with the values from the enum
+	}
 }
