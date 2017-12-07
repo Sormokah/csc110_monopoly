ea05444c5abe607dda17a9d377f607aa8c863338
Dcraw139 <DCrawford@student.neumont.edu>
2017-12-06 21:05:22 -0700

    chest and chance are in their final forms!

--- src/csc110_monopoly/Chance.java
+++ src/csc110_monopoly/Chance.java
@@ -3,186 +3,210 @@ package csc110_monopoly;
 import java.util.ArrayList;
 import java.util.Random;
 
-import com.oracle.xmlns.internal.webservices.jaxws_databinding.SoapBindingParameterStyle;
 
 public class Chance {
 	ArrayList<Integer> cards = new ArrayList<>();
+	ArrayList<Integer> picked = new ArrayList<>();
 	int maxCards = 16;
 	Banker bank = new Banker();
-	
+
 	public void deckChanceCards() {
-		for(int i = 0; i < maxCards; i++) {
+		for (int i = 0; i < maxCards; i++) {
 			cards.add(i);
+			picked.remove(i);
+			maxCards++;
 		}
 	}
-	
+
 	public void pickChance(Player activePlayer) {
-	Random gen = new Random();
-	if(maxCards == 0) {
-		deckChanceCards();
-		ArrayList<Player> activePlayers = Monopoly.g1.getPlayers();
-		for(Player p : activePlayers) {
-			if (p.isHasGetOutOfJailFreeChance()) {
-				cards.remove(6);
-				maxCards--;
-				break;
+		Random gen = new Random();
+		CommunityChest chest = new CommunityChest();
+		if (maxCards == 0) {
+			deckChanceCards();
+			ArrayList<Player> activePlayers = Monopoly.g1.getPlayers();
+			for (Player p : activePlayers) {
+				if (p.isHasGetOutOfJailFreeChance()) {
+					picked.add(6);
+					maxCards--;
+					break;
+				}
 			}
 		}
-	}
-	int card = gen.nextInt(maxCards);
-	
-	if(card == 0) {
-		System.out.println("Advance to Go (Collect $200)");
-		activePlayer.setLocation(0);
-		bank.pay(200, activePlayer);
-		cards.remove(0);
-		maxCards--;
-	}
-	if(card == 1) {
-		System.out.println("Advance to Illinois Ave. - If you pass Go, collect $200");
-		if (activePlayer.getLocation() > 24) {
-		bank.pay(200 , activePlayer);
+
+		boolean validPick = false;
+		
+		int card = 0;
+
+		while (!validPick) {
+			card = gen.nextInt(maxCards);
+			validPick = validPicker(validPick, card);
+
 		}
-		 activePlayer.setLocation(24);
-//		if board[24] is owned pay owner.
-		cards.remove(1);
-		maxCards--;
-	}
-	if(card == 2) {
-		System.out.println("Advance to St. Charles Place - If you pass Go, collect $200");
-		if (activePlayer.getLocation() > 11) {
-		bank.pay(200 , activePlayer);
+
+		if (card == 0) {
+			System.out.println("Advance to Go (Collect $200)");
+			activePlayer.setLocation(0);
+			bank.pay(200, activePlayer);
+			picked.add(0);
+			maxCards--;
 		}
-		//if board[11] is owned pay owner.
-		activePlayer.setLocation(11);
-		cards.remove(2);
-		maxCards--;
-	}
-	if(card == 3) {
-		System.out.println("Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times the amount thrown.");
-		if (activePlayer.getLocation() == 7) { 
-		activePlayer.setLocation(12);
-		maxCards--;
+		if (card == 1) {
+			System.out.println("Advance to Illinois Ave. - If you pass Go, collect $200");
+			if (activePlayer.getLocation() > 24) {
+				bank.pay(200, activePlayer);
+			}
+			activePlayer.setLocation(24);
+			// if board[24] is owned pay owner.
+			picked.add(1);
+			maxCards--;
 		}
-		if (activePlayer.getLocation() == 36) {
-		 bank.pay(200 , activePlayer);
-		activePlayer.setLocation(12);
-		maxCards--;
+		if (card == 2) {
+			System.out.println("Advance to St. Charles Place - If you pass Go, collect $200");
+			if (activePlayer.getLocation() > 11) {
+				bank.pay(200, activePlayer);
+			}
+			// if board[11] is owned pay owner.
+			activePlayer.setLocation(11);
+			picked.add(2);
+			maxCards--;
 		}
-		if (activePlayer.getLocation() == 22) {
-		activePlayer.setLocation(28);
-		maxCards--;
+		if (card == 3) {
+			System.out.println(
+					"Advance token to nearest Utility. If unowned, you may buy it from the Bank. If owned, throw dice and pay owner a total ten times the amount thrown.");
+			if (activePlayer.getLocation() == 7) {
+				activePlayer.setLocation(12);
+				maxCards--;
+			}
+			if (activePlayer.getLocation() == 36) {
+				bank.pay(200, activePlayer);
+				activePlayer.setLocation(12);
+				maxCards--;
+			}
+			if (activePlayer.getLocation() == 22) {
+				activePlayer.setLocation(28);
+				maxCards--;
+			}
+			// if activePlayer.getLocation() == owned pay owner 10x (2d6)
+			picked.add(3);
 		}
-//		if activePlayer.getLocation() == owned pay owner 10x (2d6)
-		cards.remove(3);
-	}
-	if(card == 4) {
-		System.out.println("Advance token to the nearest Railroad and pay owner twice the rental to which he/she is otherwise entitled. If Railroad is unowned, you may buy it from the Bank.");
-		if (activePlayer.getLocation() == 36) {
-		bank.pay(200 , activePlayer);
-		activePlayer.setLocation(5);
-		maxCards--;
+		if (card == 4) {
+			System.out.println(
+					"Advance token to the nearest Railroad and pay owner twice the rental to which he/she is otherwise entitled. If Railroad is unowned, you may buy it from the Bank.");
+			if (activePlayer.getLocation() == 36) {
+				bank.pay(200, activePlayer);
+				activePlayer.setLocation(5);
+				maxCards--;
+			}
+			if (activePlayer.getLocation() == 7) {
+				activePlayer.setLocation(15);
+				maxCards--;
+			}
+			if (activePlayer.getLocation() == 22)
+				activePlayer.setLocation(25);
+			maxCards--;
+			// pay the person if location is owned
+			picked.add(4);
 		}
-		if (activePlayer.getLocation() == 7) {
-		activePlayer.setLocation(15);
-		maxCards--;
+		if (card == 5) {
+			System.out.println("Bank pays you dividend of $50");
+			bank.pay(50, activePlayer);
+			maxCards--;
+			picked.add(5);
 		}
-		if (activePlayer.getLocation() == 22)
-		activePlayer.setLocation(25);
-		maxCards--;
-//		pay the person if location is owned
-		cards.remove(4);
-	}
-	if(card == 5) {
-		System.out.println("Bank pays you dividend of $50");
-		bank.pay(50 , activePlayer);
-		maxCards--;
-		cards.remove(5);
-	}
-	if(card == 6) {
-		System.out.println("Get out of Jail Free – This card may be kept until needed, or traded/sold");
-		activePlayer.setHasGetOutOfJailFreeChance(true);
-		maxCards--;
-		cards.remove(6);
-	}
-	if(card == 7) {
-		System.out.println("Go Back 3 Spaces");
-		if (activePlayer.getLocation() == 7) {
-		activePlayer.setLocation(4);
-		maxCards--; //&& IncomeTax();
+		if (card == 6) {
+			System.out.println("Get out of Jail Free – This card may be kept until needed, or traded/sold");
+			activePlayer.setHasGetOutOfJailFreeChance(true);
+			maxCards--;
+			picked.add(6);
 		}
-		if (activePlayer.getLocation() == 22) {
-		activePlayer.setLocation(19); 
-		maxCards--;
-//		if board[19] == owned pay rent to owner.
+		if (card == 7) {
+			System.out.println("Go Back 3 Spaces");
+			if (activePlayer.getLocation() == 7) {
+				activePlayer.setLocation(4);
+				maxCards--;
+				// IncomeTax(activePlayer);
+			}
+			if (activePlayer.getLocation() == 22) {
+				activePlayer.setLocation(19);
+				maxCards--;
+				// if board[19] == owned pay rent to owner.
+			}
+			if (activePlayer.getLocation() == 36) {
+				activePlayer.setLocation(33);
+				maxCards--;
+				chest.pickChest(activePlayer);
+			}
+			picked.add(7);
 		}
-		if (activePlayer.getLocation() == 36) { 
-		activePlayer.setLocation(33);
-		maxCards--;
+
+		if (card == 8) {
+			System.out.println("Go to Jail – Go directly to Jail – Do not pass Go, do not collect $200");
+			activePlayer.setLocation(10);
+			activePlayer.setInJail(true);
+			maxCards--;
+			picked.add(8);
 		}
-//		Community.pickChest();
-		cards.remove(7);
+		if (card == 9) {
+			System.out.println(
+					"Make general repairs on all your property – For each house pay $25 – For each hotel $100");
+			bank.payBank((activePlayer.getHouses() * 25), activePlayer);
+			bank.payBank((activePlayer.getHotels() * 100), activePlayer);
+			maxCards--;
+			picked.add(9);
 		}
-	
-	if(card == 8) {
-		System.out.println("Go to Jail – Go directly to Jail – Do not pass Go, do not collect $200");
-		activePlayer.setLocation(10);
-		//activePlayer.isInJail = true;
-		maxCards--;
-		cards.remove(8);
-	}
-	if(card == 9) {
-		System.out.println("Make general repairs on all your property – For each house pay $25 – For each hotel $100");
-		bank.payBank((activePlayer.getHouses() * 25) , activePlayer );
-		bank.payBank((activePlayer.getHotels() * 100), activePlayer);
-		maxCards--;
-		cards.remove(9);
-	}
-	if(card == 10) {
-		System.out.println("Pay poor tax of $15");
-		bank.payBank(15 , activePlayer);
-		maxCards--;
-		cards.remove(10);
-	}
-	if(card == 11) {
-		System.out.println("Take a trip to Reading Railroad – If you pass Go, collect $200");
-		if (activePlayer.getLocation() > 5) {
-		bank.pay(200 , activePlayer);
-		activePlayer.setLocation(5);
-		maxCards--;
+		if (card == 10) {
+			System.out.println("Pay poor tax of $15");
+			bank.payBank(15, activePlayer);
+			maxCards--;
+			picked.add(10);
 		}
-		if (activePlayer.getLocation() < 5) {
-		activePlayer.setLocation(5);
-		maxCards--;
+		if (card == 11) {
+			System.out.println("Take a trip to Reading Railroad – If you pass Go, collect $200");
+			if (activePlayer.getLocation() > 5) {
+				bank.pay(200, activePlayer);
+				activePlayer.setLocation(5);
+				maxCards--;
+			}
+			if (activePlayer.getLocation() < 5) {
+				activePlayer.setLocation(5);
+				maxCards--;
+			}
+			picked.add(11);
 		}
-		cards.remove(11);
-	}
-	if(card == 12) {
-		System.out.println("Take a walk on the Boardwalk – Advance token to Boardwalk");
-		activePlayer.setLocation(39); 
-		//if board[39] is owned pay owner.
-		maxCards--;
-		cards.remove(12);
-	}
-	if(card == 13) {
-		System.out.println("You have been elected Chairman of the Board – Pay each player $50");
-		//player.bank - 50 for playersCount - 1;
-		maxCards--;
-		cards.remove(13);
-	}
-	if(card == 14) {
-		System.out.println("Your building loan matures – Collect $150");
-		bank.pay(150 , activePlayer);
-		maxCards--;
-		cards.remove(14);
-	}
-	if(card == 15) {
-		System.out.println("You have won a crossword competition - Collect $100");
-		bank.pay(100 , activePlayer);
-		maxCards--;
-		cards.remove(15);
+		if (card == 12) {
+			System.out.println("Take a walk on the Boardwalk – Advance token to Boardwalk");
+			activePlayer.setLocation(39);
+			// if board[39] is owned pay owner.
+			maxCards--;
+			picked.add(12);
+		}
+		if (card == 13) {
+			System.out.println("You have been elected Chairman of the Board – Pay each player $50");
+			// player.bank - 50 for playersCount - 1;
+			maxCards--;
+			picked.add(13);
+		}
+		if (card == 14) {
+			System.out.println("Your building loan matures – Collect $150");
+			bank.pay(150, activePlayer);
+			maxCards--;
+			picked.add(14);
+		}
+		if (card == 15) {
+			System.out.println("You have won a crossword competition - Collect $100");
+			bank.pay(100, activePlayer);
+			maxCards--;
+			picked.add(15);
+		}
 	}
 
+	public boolean validPicker(boolean validPick, int card) {
+		for (int i = 0; i < maxCards; i++) {
+			if (card == picked.get(i)) {
+				return false;
+			}
+		}
 
+		return true;
 	}
 }
--- src/csc110_monopoly/CommunityChest.java
+++ src/csc110_monopoly/CommunityChest.java
@@ -8,12 +8,15 @@ public class CommunityChest {
 	
 	Random gen = new Random();
 	ArrayList<Integer> cards = new ArrayList<>();
+	ArrayList<Integer> picked = new ArrayList<>();
 	int maxCards = 16;
 	Banker bank = new Banker();
 	
 	public void deckChestCards() {
 		for(int i = 0; i < maxCards; i++) {
 			cards.add(i);
+			picked.remove(i);
+			maxCards++;
 		}
 	}
 	
@@ -21,49 +24,61 @@ public class CommunityChest {
 	Random gen = new Random();
 	if(maxCards == 0) {
 		deckChestCards();
-		//check players for getoutofjailChest.
-		//if player.getoutofjailchest = true
-		//cards.remove(4);
-		//maxCards--;
+		ArrayList<Player> activePlayers = Monopoly.g1.getPlayers();
+		for(Player p : activePlayers) {
+			if (p.isHasGetOutOfJailFreeChest()) {
+				picked.add(4);
+				maxCards--;
+				break;
+			}
+		}
 	}
-	int card = gen.nextInt(maxCards);
 	
+	boolean validPick = false;
+	
+	int card = 0;
+
+	while (!validPick) {
+		card = gen.nextInt(maxCards);
+		validPick = validPicker(validPick, card);
+
+	}
 	if(card == 0) {
 		System.out.println("Advance to Go (Collect $200)");
 		activePlayer.setLocation(0);
 		bank.pay(200, activePlayer);
-		cards.remove(0);
+		picked.add(0);
 		maxCards--;
 	}
 	if(card == 1) {
 		System.out.println("Bank error in your favor – Collect $200");
 		bank.pay(200 , activePlayer);
-		cards.remove(1);
+		picked.add(1);
 		maxCards--;
 	}
 	if(card == 2) {
 		System.out.println("Doctor's fees – Pay $50");
 		bank.payBank(50 , activePlayer);
-		cards.remove(2);
+		picked.add(2);
 		maxCards--;
 	}
 	if(card == 3) {
 		System.out.println("From sale of stock you get $50");
 		bank.pay(50 , activePlayer);
-		cards.remove(3);
+		picked.add(3);
 		maxCards--;
 	}
 	if(card == 4) {
 		System.out.println("Get Out of Jail Free – This card may be kept until needed or sold");
 		activePlayer.setHasGetOutOfJailFreeChest(true);
-		cards.remove(4);
+		picked.add(4);
 		maxCards--;
 	}
 	if(card == 5) {
 		System.out.println("Go to Jail – Go directly to jail – Do not pass Go – Do not collect $200");
 		activePlayer.setLocation(10);
 		activePlayer.setInJail(true);
-		cards.remove(5);
+		picked.add(5);
 		maxCards--;
 	}
 	if(card == 6) {
@@ -74,64 +89,73 @@ public class CommunityChest {
 			p.setMoney(p.getMoney() - 50);
 			Monopoly.g1.getActivePlayer().setMoney(Monopoly.g1.getActivePlayer().getMoney() + 50);
 		}
-		cards.remove(6);
+		picked.add(6);
 		maxCards--;
 	}
 	if(card == 7) {
 		System.out.println("Holiday Fund matures - Receive $100");
 		bank.pay(100 , activePlayer);
-		cards.remove(7);
+		picked.add(7);
 		maxCards--;
 	}
 	if(card == 8) {
 		System.out.println("Income tax refund – Collect $20");
 		bank.pay(20 , activePlayer);
-		cards.remove(8);
+		picked.add(8);
 		maxCards--;
 	}
 	if(card == 9) {
 		System.out.println("Life insurance matures – Collect $100");
 		bank.pay(100 , activePlayer);
-		cards.remove(9);
+		picked.add(9);
 		maxCards--;
 	}
 	if(card == 10) {
 		System.out.println("Pay hospital fees of $100");
 		bank.payBank(100 , activePlayer);
-		cards.remove(10);
+		picked.add(10);
 		maxCards--;
 	}
 	if(card == 11) {
 		System.out.println("Pay school fees of $150");
 		bank.payBank(150 , activePlayer);
-		cards.remove(11);
+		picked.add(11);
 		maxCards--;
 	}
 	if(card == 12) {
 		System.out.println("Receive $25 consultancy fee");
 		bank.pay(25 , activePlayer);
-		cards.remove(12);
+		picked.add(12);
 		maxCards--;
 	}
 	if(card == 13) {
 		System.out.println("You are assessed for street repairs – $40 per house – $115 per hotel");
 		bank.payBank((activePlayer.getHouses() * 40) , activePlayer);
 		bank.payBank((activePlayer.getHotels() * 115) , activePlayer);
-		cards.remove(13);
+		picked.add(13);
 		maxCards--;
 	}
 	if(card == 14) {
 		System.out.println("You have won second prize in a beauty contest – Collect $10");
 		bank.pay(10 , activePlayer);
-		cards.remove(14);
+		picked.add(14);
 		maxCards--;
 	}
 	if(card == 15) {
 		System.out.println("You inherit $100");
 		bank.pay(100 , activePlayer);
-		cards.remove(15);
+		picked.add(15);
 		maxCards--;
 	}
 
+	}
+	public boolean validPicker(boolean validPick, int card) {
+		for (int i = 0; i < maxCards; i++) {
+			if (card == picked.get(i)) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 }
